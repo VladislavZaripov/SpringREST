714f267f3d82c882a372a658dbd95888b671324b
VladislavZaripov <zaripovva@mail.ru>
2021-12-09 22:48:34 +0300

    Initial commit

---
+++ src/main/java/app/WebInitializer.java
@@ -0,0 +1,22 @@
+package app;
+
+import app.configuration.DataServiceConfig;
+import app.service.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{DataServiceConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
